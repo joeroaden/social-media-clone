696db1f8112a40e2a5bca980b1dbc824b4830894
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-09-06 11:59:00 -0700

    update app.js to show header and update header to include Home, Notifcations and messages

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar"
+// import SearchBox from "./SearchBox"
+
+function Header(){
+  return (
+    <React.Fragment>
+      <Navbar/>
+      {/* <SearchBox/> */}
+    </React.Fragment>
+  );
+}
+
+export default Header;
